98828fcdadd07da36528c4e15abc031f25381c9b
Александр <Nitlenav@gmail.com>
2021-09-26 11:10:48 +0300

    Решение задания - 02 Исправление

--- task-02/src/main/java/ru/Main.java
+++ task-02/src/main/java/ru/Main.java
@@ -1,26 +1,35 @@
 package ru;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import ru.answer.AskQuestion;
-import ru.domain.Questions;
+import ru.answer.AskQuestionImpl;
+import ru.domain.QuestionsService;
+import ru.result.DisplayResult;
+import ru.result.DisplayResultImpl;
 
 import java.util.List;
 import java.util.Map;
 
 @Configuration
 @ComponentScan
+@PropertySource("classpath:questions.properties")
 public class Main {
+
     public static void main(String[] args) {
         // TODO: создайте здесь класс контекста
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        Questions questions = context.getBean(Questions.class);
+        
+        QuestionsService questionsService = context.getBean(QuestionsService.class);
+        questionsService.loadQuestions();
+
         AskQuestion askQuestion = context.getBean(AskQuestion.class);
+        askQuestion.answerQuestions();
 
-        List<String> questionsList = questions.getQuestions();
-        Map<String, String> questionAnswerMap = questions.getQuestionAnswerMap();
-
-//        questionsList.forEach(System.out::println);
+        DisplayResult displayResult = context.getBean(DisplayResult.class);
+        displayResult.showResult();
     }
 }
